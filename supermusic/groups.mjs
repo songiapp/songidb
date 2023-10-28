62864b4f098af4c8b78f9555453872af7b0eadd1
Jan Prochazka <jan.prochazka@gmail.com>
2023-10-28 17:07:12 +0200

    sumu yellow groups

--- supermusic/groups.mjs
+++ supermusic/groups.mjs
@@ -35,13 +35,17 @@ for (const char of alphabet) {
   console.log("Processing groups from:", char);
   const resp = await fetch(`https://supermusic.cz/skupiny.php?od=${char}`);
   const text = await resp.text();
-  const matches = [...text.matchAll(/\"skupina.php\?idskupiny=[^\"]+\"/g)].map(
-    (x) => x[0]
-  );
-  for (const link of matches) {
+  const matches = [
+    ...text.matchAll(
+      /\<a\s*(class\=interpretzoznam)?\s*id\="\d+"\s+href\=\"skupina.php\?idskupiny=[^\"]+\"/g
+    ),
+  ].map((x) => x[0]);
+  for (const anchor of matches) {
+    const link = anchor.match(/\"skupina.php\?idskupiny=[^\"]+\"/)[0];
     const m = link.match(/idskupiny=([\d]+)\&name=([^\"]+)/);
     const id = m[1];
     const name = m[2];
+    const isYellow = !anchor.includes("class=interpretzoznam");
     if (groupsById[id]) {
       continue;
     }
@@ -49,6 +53,7 @@ for (const char of alphabet) {
       id,
       name,
       link: link.slice(1, -1),
+      isYellow,
     };
   }
 }
