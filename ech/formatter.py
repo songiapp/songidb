bbe36c233f23de50f1bb2ec6e5bd6059c0cd16ee
Jan Prochazka <jan.prochazka@gmail.com>
2023-11-18 18:40:18 +0100

    crawled ech

---
+++ ech/formatter.py
@@ -0,0 +1,12 @@
+from scrapy_formatter import ScrapyFormatter
+
+class Formatter(ScrapyFormatter):
+    def process_artist(self, **kwargs):
+        self.add_artist(kwargs['artistName'], kwargs['artistHref'][1:])
+
+    def process_song(self, **kwargs):
+        self.add_song(
+            kwargs['songHref'][1:].replace('/', '-'),
+            kwargs['artistHref'][1:],
+            kwargs['songTitle'],
+            self.fix_chord_lines(kwargs['songText']))
